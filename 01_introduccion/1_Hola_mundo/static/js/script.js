f58e30c3f8cdf541f790ba730cbfd32ac77b749e
benjamin carrasco <benjamincarrasco@liceovvh.cl>
2026-04-15 08:12:33 -0400

    s

---
+++ 01_introduccion/1_Hola_mundo/static/js/script.js
@@ -0,0 +1,11 @@
+console.log("conexion exitosa con js");
+// 1) hola munndo con variable y const
+const saludo = "hola a todos, ";
+let destinatario = "estudiantes";
+console.log(saludo + "para todos " + destinatario);
+// impresion de saludo concatenando con (+)
+
+//2) saludar a una persona ingresada por teclado.
+let nombre = prompt("ingresa tu nombre: ");
+//Imprimir un nombre ingresado con un promt
+console.log("hola, " + nombre);
---
+++ 02_Tipos_de_datos/1.-tipo_texto/static/js/script.js
@@ -0,0 +1,8 @@
+console.log("conexion con js correcta");
+/* 
+concentenacion de texto con el signo + podemos unir texto y variables 
+*/
+const nombre = "Juan";
+const apellido = "Perez";
+//unimos ambas constantes con un texto extra.
+console.log("hola, mi nombre es " + nombre + " " + apellido);
